ec9c7d3d754a91bb433a0ff5d4aeb6fc298290e3
alihoodrichz@gmail.com <alihoodrichz@gmail.com>
2023-11-13 15:57:40 +0100

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIyNjA2MQ/spectral.js?branch_slug=main&token=6d237dd6-e5b0-48cc-863a-06c848d138c5";
+export default { extends: ruleset };
